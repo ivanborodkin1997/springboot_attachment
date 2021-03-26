e1ff03dadc55543f643ccbba49484ada56e5ebec
ivanborodkin1997 <boroda0360@mail.ru>
2021-03-26 18:32:05 +0300

    [26.03.2021; Springboot_attachment; committer: Ivan Borodkin - Added package service and user identification];

--- src/main/java/com/example/springboot/controller/MainController.java
+++ src/main/java/com/example/springboot/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.domain.Message;
+import com.example.springboot.domain.User;
 import com.example.springboot.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,9 +31,11 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag,
+    public String add(@AuthenticationPrincipal User user,
+                      @RequestParam String text,
+                      @RequestParam String tag,
                       Map<String,Object> model) {
-        Message message = new Message(text,tag);
+        Message message = new Message(text,tag, user);
         messageRepo.save(message);
         Iterable<Message> messages =messageRepo.findAll();
         model.put("messages",messages);
